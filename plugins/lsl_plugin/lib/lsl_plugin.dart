a2042c8d1d8bef5317a9f721b339f3ae0d12b40c
olafuroskar <ol.os.om.97@gmail.com>
2025-03-05 21:24:32 +0100

    outlet init

--- plugins/lsl_plugin/lib/lsl_plugin.dart
+++ plugins/lsl_plugin/lib/lsl_plugin.dart
@@ -1,47 +1,67 @@
-
-import 'dart:async';
 import 'dart:ffi';
 import 'dart:io';
-import 'dart:isolate';
+
+import 'package:android_multicast_lock/android_multicast_lock.dart';
 
 import 'lsl_plugin_bindings_generated.dart';
+import 'package:ffi/ffi.dart';
 
-/// A very short-lived native function.
-///
-/// For very short-lived functions, it is fine to call them on the main isolate.
-/// They will block the Dart execution while running the native function, so
-/// only do this for native functions which are guaranteed to be short-lived.
-int sum(int a, int b) => _bindings.sum(a, b);
+class Outlet {
+  Outlet(
+    String streamName,
+    String streamType,
+    String streamSourceId,
+  ) {
+    final name = streamName.toNativeUtf8().cast<Char>();
+    final type = streamType.toNativeUtf8().cast<Char>();
+    final lsl_channel_format_t channelFormat = lsl_channel_format_t.cft_int32;
+    final sourceId = streamSourceId.toNativeUtf8().cast<Char>();
+    streamInfo = _bindings.lsl_create_streaminfo(
+        name, type, 8, 100, channelFormat, sourceId);
 
-/// A longer lived native function, which occupies the thread calling it.
-///
-/// Do not call these kind of native functions in the main isolate. They will
-/// block Dart execution. This will cause dropped frames in Flutter applications.
-/// Instead, call these native functions on a separate isolate.
-///
-/// Modify this to suit your own use case. Example use cases:
-///
-/// 1. Reuse a single isolate for various different kinds of requests.
-/// 2. Use multiple helper isolates for parallel execution.
-Future<int> sumAsync(int a, int b) async {
-  final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
-  final int requestId = _nextSumRequestId++;
-  final _SumRequest request = _SumRequest(requestId, a, b);
-  final Completer<int> completer = Completer<int>();
-  _sumRequests[requestId] = completer;
-  helperIsolateSendPort.send(request);
-  return completer.future;
+    MulticastLock().acquire();
+    outlet = _bindings.lsl_create_outlet(streamInfo, 0, 360);
+  }
+
+  late final Pointer<lsl_streaminfo_struct_> streamInfo;
+  late final Pointer<lsl_outlet_struct_> outlet;
+  bool _isDestroyed = false;
+
+  void destroy() {
+    _bindings.lsl_destroy_outlet(outlet);
+    _bindings.lsl_destroy_streaminfo(streamInfo);
+    MulticastLock().release();
+    _isDestroyed = true;
+  }
+
+  bool isDestroyed() {
+    return _isDestroyed;
+  }
+
+  void pushSample(List<double> sample) {
+    final nativeSamplePointer =
+        malloc.allocate<Float>(sample.length * sizeOf<Float>());
+
+    for (var i = 0; i < sample.length; i++) {
+      nativeSamplePointer[i] = sample[i];
+    }
+
+    _bindings.lsl_push_sample_f(outlet, nativeSamplePointer);
+
+    malloc.free(nativeSamplePointer);
+  }
 }
 
 const String _libName = 'lsl_plugin';
 
-/// The dynamic library in which the symbols for [LslPluginBindings] can be found.
+/// The dynamic library in which the symbols for [TestFfiBindings] can be found.
 final DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
     return DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
-    return DynamicLibrary.open('lib$_libName.so');
+//    return DynamicLibrary.open('lib$_libName.so');
+    return DynamicLibrary.open('liblsl.so');
   }
   if (Platform.isWindows) {
     return DynamicLibrary.open('$_libName.dll');
@@ -51,81 +71,3 @@ final DynamicLibrary _dylib = () {
 
 /// The bindings to the native functions in [_dylib].
 final LslPluginBindings _bindings = LslPluginBindings(_dylib);
-
-
-/// A request to compute `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumRequest {
-  final int id;
-  final int a;
-  final int b;
-
-  const _SumRequest(this.id, this.a, this.b);
-}
-
-/// A response with the result of `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumResponse {
-  final int id;
-  final int result;
-
-  const _SumResponse(this.id, this.result);
-}
-
-/// Counter to identify [_SumRequest]s and [_SumResponse]s.
-int _nextSumRequestId = 0;
-
-/// Mapping from [_SumRequest] `id`s to the completers corresponding to the correct future of the pending request.
-final Map<int, Completer<int>> _sumRequests = <int, Completer<int>>{};
-
-/// The SendPort belonging to the helper isolate.
-Future<SendPort> _helperIsolateSendPort = () async {
-  // The helper isolate is going to send us back a SendPort, which we want to
-  // wait for.
-  final Completer<SendPort> completer = Completer<SendPort>();
-
-  // Receive port on the main isolate to receive messages from the helper.
-  // We receive two types of messages:
-  // 1. A port to send messages on.
-  // 2. Responses to requests we sent.
-  final ReceivePort receivePort = ReceivePort()
-    ..listen((dynamic data) {
-      if (data is SendPort) {
-        // The helper isolate sent us the port on which we can sent it requests.
-        completer.complete(data);
-        return;
-      }
-      if (data is _SumResponse) {
-        // The helper isolate sent us a response to a request we sent.
-        final Completer<int> completer = _sumRequests[data.id]!;
-        _sumRequests.remove(data.id);
-        completer.complete(data.result);
-        return;
-      }
-      throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-    });
-
-  // Start the helper isolate.
-  await Isolate.spawn((SendPort sendPort) async {
-    final ReceivePort helperReceivePort = ReceivePort()
-      ..listen((dynamic data) {
-        // On the helper isolate listen to requests and respond to them.
-        if (data is _SumRequest) {
-          final int result = _bindings.sum_long_running(data.a, data.b);
-          final _SumResponse response = _SumResponse(data.id, result);
-          sendPort.send(response);
-          return;
-        }
-        throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-      });
-
-    // Send the port to the main isolate on which we can receive requests.
-    sendPort.send(helperReceivePort.sendPort);
-  }, receivePort.sendPort);
-
-  // Wait until the helper isolate has sent us back the SendPort on which we
-  // can start sending requests.
-  return completer.future;
-}();
