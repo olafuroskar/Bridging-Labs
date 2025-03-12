90aebc0c582ed1c472a9efe79ae9ba34ef945149
olafuroskar <ol.os.om.97@gmail.com>
2025-03-12 21:32:08 +0100

    Add channel format Dart enum

---
+++ plugins/lsl_plugin/lib/src/channel_format.dart
@@ -0,0 +1,27 @@
+import 'package:lsl_plugin/src/lsl_bindings_generated.dart';
+
+enum ChannelFormat {
+  float32(lsl_channel_format_t.cft_float32),
+  double64(lsl_channel_format_t.cft_double64),
+  int8(lsl_channel_format_t.cft_int8),
+  int16(lsl_channel_format_t.cft_int16),
+  int32(lsl_channel_format_t.cft_int32),
+  int64(lsl_channel_format_t.cft_int64),
+  string(lsl_channel_format_t.cft_string),
+  undefined(lsl_channel_format_t.cft_undefined);
+
+  final lsl_channel_format_t value;
+  const ChannelFormat(this.value);
+
+  static ChannelFormat fromValue(lsl_channel_format_t value) => switch (value) {
+        lsl_channel_format_t.cft_float32 => float32,
+        lsl_channel_format_t.cft_double64 => double64,
+        lsl_channel_format_t.cft_int8 => int8,
+        lsl_channel_format_t.cft_int16 => int16,
+        lsl_channel_format_t.cft_int32 => int32,
+        lsl_channel_format_t.cft_int64 => int64,
+        lsl_channel_format_t.cft_string => string,
+        lsl_channel_format_t.cft_undefined => undefined,
+        _ => throw ArgumentError(),
+      };
+}
