e537a4b7c6e31cf8aa3777918c3117366b003c74
olafuroskar <ol.os.om.97@gmail.com>
2025-03-12 21:29:25 +0100

    change output destination of generated bindings

---
+++ plugins/lsl_plugin/lib/src/liblsl.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:lsl_plugin/src/lsl_bindings_generated.dart';
+
+const String _libName = 'lsl_plugin';
+
+/// The dynamic library in which the symbols for [TestFfiBindings] can be found.
+final DynamicLibrary _dylib = () {
+  if (Platform.isMacOS || Platform.isIOS) {
+    return DynamicLibrary.open('$_libName.framework/$_libName');
+  }
+  if (Platform.isAndroid || Platform.isLinux) {
+//    return DynamicLibrary.open('lib$_libName.so');
+    return DynamicLibrary.open('liblsl.so');
+  }
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('$_libName.dll');
+  }
+  throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+}();
+
+/// The bindings to the native functions in [_dylib].
+final LslPluginBindings bindings = LslPluginBindings(_dylib);
