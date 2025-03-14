02370a33e4580948cf70aee4cc5aeb05a0fb4930
olafuroskar <ol.os.om.97@gmail.com>
2025-03-14 09:37:01 +0100

    add tests

--- plugins/lsl_plugin/test/outlet_test.dart
+++ plugins/lsl_plugin/test/outlet_test.dart
@@ -30,6 +30,10 @@ void main() {
     StreamInfo.setBindings(mockLsl);
     IntOutlet.setBindings(mockLsl);
     IntOutlet.setMulticastLock(mockMulticastLock);
+    DoubleOutlet.setBindings(mockLsl);
+    DoubleOutlet.setMulticastLock(mockMulticastLock);
+    StringOutlet.setBindings(mockLsl);
+    StringOutlet.setMulticastLock(mockMulticastLock);
 
     // Mockito does not know how to make dummy native values so we must provide them
     streamInfoPointer = malloc.allocate<lsl_streaminfo_struct_>(
@@ -46,35 +50,147 @@ void main() {
     malloc.free(outletPointer);
   });
 
-  test(
-      "Setting the channel format to a floating point format on an integer outlet should cause a runtime error",
-      () {
-    final outletBuilder = IntOutletBuilder();
-    outletBuilder.name = "Test";
-    outletBuilder.type = "EEG";
-    outletBuilder.sourceId = "Testing Id";
-    outletBuilder.channelFormat = ChannelFormat.float32;
-    final result = outletBuilder.build();
+  group('Integer outlets', () {
+    test(
+        "Setting the channel format to a floating point format should cause a runtime error",
+        () {
+      final outletBuilder = IntOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.float32;
+      final result = outletBuilder.build();
 
-    expect(result is Error, true);
+      expect(result is Error, true);
+    });
+
+    test(
+        "Setting the channel format to an 8 bit integer should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = IntOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.int8;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
+
+    test(
+        "Setting the channel format to an 16 bit integer should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = IntOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.int16;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
+
+    test(
+        "Setting the channel format to an 32 bit integer should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = IntOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.int32;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
+
+    test(
+        "Setting the channel format to an 64 bit integer should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = IntOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.int64;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
   });
 
-  test(
-      "Setting the channel format to a 8 bit integer format on an integer outlet should be ok",
-      () {
-    final outletBuilder = IntOutletBuilder();
-    outletBuilder.name = "Test";
-    outletBuilder.type = "EEG";
-    outletBuilder.sourceId = "Testing Id";
-    outletBuilder.channelFormat = ChannelFormat.int8;
-    final outlet = outletBuilder.build();
+  group("Double outlets", () {
+    test(
+        "Setting the channel format to an 64 bit integer cause a runtime error",
+        () {
+      final outletBuilder = DoubleOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.int64;
+      final result = outletBuilder.build();
 
-    switch (outlet) {
-      case Ok(value: var outlet):
-        var result = outlet.pushSample([1, 3, 4, 5]);
-        expect(result is Ok, true);
-      default:
-        fail("Should not fail");
-    }
+      expect(result is Error, true);
+    });
+
+    test(
+        "Setting the channel format to a 32 bit float should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = DoubleOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.float32;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
+
+    test(
+        "Setting the channel format to a 64 bit double should be ok, and pushing a valid sample should work",
+        () {
+      final outletBuilder = DoubleOutletBuilder();
+      outletBuilder.name = "Test";
+      outletBuilder.type = "EEG";
+      outletBuilder.sourceId = "Testing Id";
+      outletBuilder.channelFormat = ChannelFormat.double64;
+      final outlet = outletBuilder.build();
+
+      switch (outlet) {
+        case Ok(value: var outlet):
+          var result = outlet.pushSample([1, 3, 4, 5]);
+          expect(result is Ok, true);
+        default:
+          fail("Should not fail");
+      }
+    });
   });
 }
