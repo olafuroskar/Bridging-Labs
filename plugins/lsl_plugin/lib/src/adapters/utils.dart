02fb9385daa9914711cb90e04c1e7516b2d3fdd1
olafuroskar <ol.os.om.97@gmail.com>
2025-04-13 11:12:42 +0200

    revert

--- plugins/lsl_plugin/lib/src/adapters/utils.dart
+++ plugins/lsl_plugin/lib/src/adapters/utils.dart
@@ -1,21 +1,19 @@
 import 'dart:ffi';
 
-void checkError(Pointer<Int32> ec, {Object? context}) {
+void checkError(Pointer<Int32> ec) {
   if (ec[0] < 0) {
     switch (ec[0]) {
       case -1:
-        throw Exception(
-            "The operation failed due to a timeout. Context: $context");
+        throw Exception("The operation failed due to a timeout.");
       case -2:
-        throw Exception("The stream has been lost. Context: $context");
+        throw Exception("The stream has been lost.");
       case -3:
         throw Exception(
-            "An argument was incorrectly specified (e.g., wrong format or wrong length). Context: $context");
+            "An argument was incorrectly specified (e.g., wrong format or wrong length).");
       case -4:
-        throw Exception(
-            "An internal internal error has occurred. Context: $context");
+        throw Exception("An internal internal error has occurred.");
       default:
-        throw Exception("An unknown error has occurred. Context: $context");
+        throw Exception("An unknown error has occurred.");
     }
   }
 }
