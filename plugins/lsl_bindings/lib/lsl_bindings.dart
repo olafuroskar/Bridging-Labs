0f3d09fff57f4adfe5ad1397154f29824fda6a6d
olafuroskar <ol.os.om.97@gmail.com>
2025-04-16 13:13:11 +0200

    move bindings to seperate plugin

---
+++ plugins/lsl_bindings/lib/lsl_bindings.dart
@@ -0,0 +1,9 @@
+/// A library for interacting with the Lab Streaming Layer
+library;
+
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:lsl_bindings/src/lsl_bindings_generated.dart';
+
+part 'src/lsl_library.dart';
