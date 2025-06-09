9a64d024cc2f0f9179a58c2f185eaaec1d8ef93f
olafuroskar <ol.os.om.97@gmail.com>
2025-06-09 12:03:54 +0200

    apple build script

--- scripts/lsl_apple.sh
+++ scripts/lsl_apple.sh
@@ -21,3 +21,34 @@ rm -rf ../apple/liblsl-1.16/.github
 
 echo "🧹 Cleaning up submodule"
 git restore .
+
+cd ../apple/liblsl-1.16
+
+echo "Building for iOS"
+# For iOS devices
+cmake -G "Xcode" -B build/ios -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES="arm64"
+
+echo "Building for iOS simulators"
+# For iOS simulators
+cmake -G "Xcode" -B build/ios-sim \
+  -DCMAKE_SYSTEM_NAME=iOS \
+  -DCMAKE_OSX_SYSROOT=iphonesimulator \
+  -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
+
+echo "Building for macOS"
+# For macOS devices
+cmake -G "Xcode" -B build/macos -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+
+echo "Building release"
+# Build Release configuration. Run
+cmake --build build/ios --config Release
+cmake --build build/ios-sim --config Release
+cmake --build build/macos --config Release
+
+echo "🍎 Creating framework"
+# Then create an xcframework by running
+xcodebuild -create-xcframework \
+  -framework build/macos/Release/lsl.framework \
+  -framework build/ios/Release-iphoneos/lsl.framework \
+  -framework build/ios-sim/Release-iphonesimulator/lsl.framework \
+  -output xcframeworks/lsl.xcframework
