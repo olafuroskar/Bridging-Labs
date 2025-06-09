770e4e1006fba6a82ab05bad7cb66cf5ca2100e7
olafuroskar <ol.os.om.97@gmail.com>
2025-06-09 11:23:39 +0200

    use 1.16.2

--- scripts/lsl_add_skip_install.sh
+++ scripts/lsl_add_skip_install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "$(pwd)"
-
 # Ensure the script is run from the workspace root
 if [ ! -f "pubspec.yaml" ]; then
   echo "❌ Please run this script from the root of the workspace (where pubspec.yaml is)."
   exit 1
 fi
 
-echo "✅ Updating liblsl submodule..."
-cd plugins/lsl_bindings/src
-git checkout master
-git pull origin master
+cd liblsl
 
 echo "📦 Applying patch..."
-git apply ../../../patches/lsl_skip_install.patch
+git apply ../patches/lsl_skip_install.patch
 
 echo "✅ liblsl successfully updated and patched."
+
+echo "🖨️ Copying patched liblsl to lsl_bindings"
+cp -r . ../plugins/lsl_bindings/src
+rm -rf ../plugins/lsl_bindings/src/.git
+
+echo "🧹 Cleaning up submodule"
+git restore .
