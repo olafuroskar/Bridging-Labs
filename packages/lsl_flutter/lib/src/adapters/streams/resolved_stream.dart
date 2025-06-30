87e95f6937e1b749921e325834649e9d04b06b1b
olafuroskar <ol.os.om.97@gmail.com>
2025-06-30 15:27:00 +0200

    bump

--- packages/lsl_flutter/lib/src/adapters/streams/resolved_stream.dart
+++ packages/lsl_flutter/lib/src/adapters/streams/resolved_stream.dart
@@ -6,7 +6,12 @@ import 'package:lsl_flutter/lsl_flutter.dart';
 /// We use a constant class for passing back the id, instead of simply returning an int
 /// to emphasize the immutability of the id.
 class ResolvedStream {
+  /// Reference to the native stream info object
   final lsl_streaminfo streamInfoPointer;
+
+  /// The Dart StreamInfo object corresponding to [streamInfoPointer]
   final StreamInfo info;
+
+  /// Creates a [ResolvedStream] instance
   ResolvedStream(this.streamInfoPointer, this.info);
 }
